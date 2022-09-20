93329f474ffcf1a3648f0e0be5d2b83dbef0307e
mlr <mrhoads1@gmail.com>
2022-09-20 09:22:02 -0500

    building request

--- src/lego_api.py
+++ src/lego_api.py
@@ -3,22 +3,27 @@ Connect to rebrickable rest api and return data by set number.
 """
 
 import os
-import pandas
+import pandas as pd
+from requests.auth import HTTPBasicAuth
 import requests
 
-# required to load in .env during python execution
 from dotenv import load_dotenv
 
 # load api key
 load_dotenv()
 KEY = os.getenv("REBRICKABLE_KEY", default="NO_KEY")
-print(KEY)
 
 # update set list
 
 # load set numbers to list from pandas
+sets = pd.read_csv("./input_data/sets.csv")["set_num"].tolist()
 
 # build request url
+url = 'https://rebrickable.com/api/v3/lego/themes/'
+headers = {'Authorization': "key " + KEY}
+req = requests.get(url, headers=headers)
+print(req.url)
+print(req)
 
 # start request
 
