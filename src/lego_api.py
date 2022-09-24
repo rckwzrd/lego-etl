2cfc0e6dfeb50ace2e4c988e79858b660794d6f1
mlr <mrhoads1@gmail.com>
2022-09-24 18:20:19 -0500

    working api request

--- src/lego_api.py
+++ src/lego_api.py
@@ -4,13 +4,12 @@ Connect to rebrickable rest api and return data by set number.
 API does not have parameters to embedded a set list payload into the call. 
 
 Have to retrieve details for each set individually and use a short sleep to follow rules.
-
-There may be a way to filter on a list of sets.
 """
 
 import os
 import json
-import pandas as pd
+import time
+import pandas
 import requests
 
 from dotenv import load_dotenv
@@ -19,31 +18,40 @@ from dotenv import load_dotenv
 load_dotenv()
 KEY = os.getenv("REBRICKABLE_KEY")
 
-# load set ids and build list of urls
-set_ids = pd.read_csv("./input_data/sets.csv")["set_num"].tolist()
-base_url = 'https://rebrickable.com/api/v3/lego/sets/'
-set_urls = [base_url+ids for ids in set_ids]
- 
-# build request
-for url in set_urls:
-    # make request
-    # extract data
-    # sleep 1.1sec
-    pass
+# load set ids
+set_ids = pandas.read_csv("./input_data/sets.csv")["set_num"].tolist()
 
+# set headers
 headers = {
         "Accept": "application/json",
         "Authorization": "key " + KEY
 }
-req = requests.get(url="https://rebrickable.com/api/v3/lego/sets/60181-1", headers=headers)
-print(req.url)
-print(type(req.json()))
-print(req.ok)
-print(req.status_code)
 
+# helper to get run request
+def run_request(set_id):
+    url = f"https://rebrickable.com/api/v3/lego/sets/{set_id}"
+    req = requests.get(url=url, headers=headers)
+    time.sleep(1.1)
+    return req.json()
 
+# request data for each set
+set_data = [run_request(set_id) for set_id in set_ids[0:5]]
+print(set_data)
+
+# load to df
+
+# print head
+
+# count sets and sum parts
+
+# request set
+#req = requests.get(url="https://rebrickable.com/api/v3/lego/sets/60181-1", headers=headers)
+#print(req.url)
+#print(type(req.json()))
+#print(req.ok)
+#print(req.status_code)
 
 # request themes
-url = 'https://rebrickable.com/api/v3/lego/themes/'
-headers = {'Authorization': "key " + KEY}
-req = requests.get(url, headers=headers)
+#url = 'https://rebrickable.com/api/v3/lego/themes/'
+#headers = {'Authorization': "key " + KEY}
+#req = requests.get(url, headers=headers)
