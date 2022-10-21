15e1eec1408fa18c7cd281a5d067c7bd45b5f4a2
mlr <mrhoads1@gmail.com>
2022-10-21 16:05:27 -0500

    working lego sqlite etl

---
+++ src/lego_etl.py
@@ -0,0 +1,23 @@
+# import sqlite
+# import get_sets
+# import get_themes
+
+# make database
+
+# connect to database
+
+# make set table, set types and key
+
+# make theme table, set types and key
+
+# load single set
+
+# load single theme
+
+# back up
+
+# close connection
+
+# proper string formatting
+
+
