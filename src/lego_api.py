c84afb6204ee2ac6d05e9761494beca37f683dee
mlr <mrhoads1@gmail.com>
2022-12-13 11:50:54 -0600

    loading themes

--- src/lego_api.py
+++ src/lego_api.py
@@ -20,13 +20,14 @@ load_dotenv()
 KEY = os.getenv("KEY")
 
 # load set ids
-set_ids = pandas.read_csv("./input_data/sets.csv")["set_num"].tolist()
+# set_ids = pandas.read_csv("./input_data/sets.csv")["set_num"].tolist()
 
 # set headers
 headers = {
         "Accept": "application/json",
         "Authorization": "key " + KEY
 }
+
 
 # helper to run request
 def get_sets(set_id):
@@ -35,33 +36,37 @@ def get_sets(set_id):
     time.sleep(1.1)
     return req.json()
 
-# helper run theme request
+
+# helper to run theme request
 def get_themes():
-    url = "https://rebrickable.com/api/v3/lego/themes/?page=1&page_size=500"
+    url = "https://rebrickable.com/api/v3/lego/themes/?page=1&page_size=1000"
     req = requests.get(url=url, headers=headers)
     time.sleep(1.1)
     return req.json()["results"]
 
-if "__name__" == "__main":
+
+if __name__ == "__main__":
 
     # request data for each set
-    set_data = [get_sets(set_id) for set_id in set_ids]
+    # set_data = [get_sets(set_id) for set_id in set_ids]
     
     # load to df
-    set_df = pandas.DataFrame(set_data)
+    # set_df = pandas.DataFrame(set_data)
     
     # request themes
-    themes_data = pandas.DataFrame(get_themes())
+    # themes_data = pandas.DataFrame(get_themes())
+    themes_data = get_themes()
     
     # join sets to themes and save
-    join_data = set_df.merge(themes_data, left_on="theme_id", right_on="id", validate="m:1")
-    join_data.to_csv("output_data/join_data.csv")
+    # join_data = set_df.merge(themes_data, left_on="theme_id", right_on="id", validate="m:1")
+    # join_data.to_csv("output_data/join_data.csv")
     
     # look at head of df
-    print(join_data.head())
+    # print(join_data.head())
     
     # count sets, parts, and themes
-    print(set_df["num_parts"].sum())
-    print(set_df["num_parts"].count())
-    print(join_data["name_y"].value_counts())
+    # print(set_df["num_parts"].sum())
+    # print(set_df["num_parts"].count())
+    # print(join_data["name_y"].value_counts())
+    print(themes_data)
 
--- src/lego_load.py
+++ src/lego_load.py
@@ -1,2 +1,12 @@
 # main module for lego etl pipeline
 # api --> parse --> db
+
+import pprint
+from lego_api import get_themes
+from lego_dp import connect_db, close_db, load_table
+
+themes = get_themes()
+pprint.pprint(themes)
+
+
+# hope to demonstrate the usefulness of modules with concise loading of themes.
